7e1879f42264f03f8099d9a67e4990f35fd4050d
mikehov <mikehovje@gmail.com>
2020-06-10 14:39:04 +0200

    Test

--- routes/router.js
+++ routes/router.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const homepage = require('./index');
 const login = require('./log-in');
 const register = require('./register');
+const swipe = require('./swipe');
 
 
 //Make routes
 router.use('/', homepage);
 router.use('/', login);
 router.use('/', register);
+router.use('/', swipe);
 
 
 module.exports = router;    
