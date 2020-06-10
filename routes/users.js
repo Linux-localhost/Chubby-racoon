570434671e4b978bf28bb839d17d55aeb06bea5f
mikehov <mikehovje@gmail.com>
2020-06-10 18:58:08 +0200

    Swipe feature changes

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../helper/database');
+
+router.get('/users', (req, res) => {
+  db.get()
+      .collection('user')
+      .find({})
+      .toArray()
+      .then((users) => {
+        console.log('users', users);
+      });
+});
+
+module.exports = router;
