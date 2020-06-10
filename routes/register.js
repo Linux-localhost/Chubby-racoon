6f21092381e884ce4dfac09a8bd7ccb163f23a79
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2020-06-10 13:28:50 +0200

    Added register & login with the database

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../helper/database');
 
 // Registeren
 router.get('/registeren', (req, res) => {
   res.render('registeren.ejs');
 });
+
+
+router.post('/register', (req, res) => {
+  const createUsername = req.body.name;
+  const createEmail = req.body.email.toLowerCase();
+  const createPassword = req.body.password;
+  const createPassword2 = req.body.password_repeat;
+
+  db.get().collection('user').findOne({
+    email: createEmail,
+  },
+  (err, user) => {
+    if (err) console.log(err);
+    if (user) {
+      console.log('Email already exist please try a diffrent one');
+      res.redirect('/registeren');
+    } else if (createPassword == createPassword2) {
+      db.get().collection('user').insertOne({
+        'username': createUsername,
+        'password': createPassword,
+        'email': createEmail,
+        'gender': '',
+        'age': Math.floor(Math.random() * 30) + 20,
+        'city': '',
+        'characteristics': [],
+        'picture': 'stock.png',
+        'likes': {},
+      });
+      console.log(`A new user has registered #awesome! : ${req.body.email}`);
+      res.redirect('/inloggen');
+    } else {
+      res.redirect('/registeren');
+    }
+  });
+});
+
 
 module.exports = router;
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({
+  extended: true,
+}));
+require('dotenv').config();
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+router.use(cookieParser());
+router.use(
+    session({
+      secret: process.env.SECRET,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 3600000,
+      },
+    }),
+);
 
 // Pages required
 const homepage = require('./index');
