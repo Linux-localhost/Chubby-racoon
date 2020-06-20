37c1b3b0a71e72d6ed1d31bd0c0526c93a7ca4ca
mikehov <mikehovje@gmail.com>
2020-06-20 13:47:01 +0200

    Notification fix

--- routes/swipe.js
+++ routes/swipe.js
@@ -6,7 +6,7 @@ const objectId = mongodb.ObjectID;
 
 
 let index = 0;
-// eerste keer van het laden van de pagina
+// first time loading the page
 router.get('/swipe', async (req, res) => {
   if (!req.session.user) {
     res.redirect('/inloggen');
@@ -21,19 +21,16 @@ router.get('/swipe', async (req, res) => {
   res.render('./swipe.ejs', {data: users[index]});
 });
 
-// na elke swipe bijhouden
+// keeps track off every swipe and goes to the next
 router.post('/swipe', async (req, res) => {
   const users = await db
       .get()
       .collection('user')
       .find({_id: {$ne: objectId(req.session.user._id)}})
       .toArray();
-  index ++;
-  if (index == users.length) {
-    index = 1;
-  }
-  console.log(index);
+  index++;
 
+  // if you superliked(1) or liked(2) that person, it will be saved
   if (req.body.liking == 1 || req.body.liking == 2) {
     await db.get().collection('user').updateOne({
       '_id': objectId(req.session.user._id),
@@ -44,24 +41,44 @@ router.post('/swipe', async (req, res) => {
     }, (err, result) => {
       if (err) console.log(err);
       if (result) {
-        console.log('Gelukt');
-        console.log('Mission accomplisched');
+        console.log('It works!');
       }
     });
+    const checkLike = await db
+        .get()
+        .collection('user')
+        // eslint-disable-next-line max-len
+        .find({_id: objectId(users[index -1]._id), likes: objectId(req.session.user._id)})
+        .toArray();
+
+    // checks if they liked you to
+    if (checkLike && checkLike.length > 0) {
+      // eslint-disable-next-line max-len
+      console.log(`You have a match with: ` + `${users[index-1].username}` + ' ' + `${users[index-1]._id}`);
+      res.render('./notification.ejs', {data: users[index -1]});
+    }
+    // if you disliked(0) that person, it will be saved
   } else {
-    // Als je wil kun je hier de dislikes bijhouden
+    await db.get().collection('user').updateOne({
+      '_id': objectId(req.session.user._id),
+    }, {
+      $push: {
+        dislikes: users[index -1]._id,
+      },
+    }, (err, result) => {
+      if (err) console.log(err);
+      if (result) {
+        console.log('Not your type off person');
+      }
+    });
   }
 
-  const checklike = await db
-      .get()
-      .collection('user')
-      .find({_id: objectId(users[index -1]._id), likes: [objectId(req.session.user._id)]})
-      .toArray();
-
-  if (checklike && checklike.length > 0) {
-    console.log(`IT IS A MATCH BETWEEN: ${req.session.user._id} AND ${users[index-1]._id}`);
+  // checks if you reaced the end of all the users
+  if (index == users.length) {
+    console.log('There are no new matches coming, you have reached the end');
+    index = 0;
+    res.render('./nomatches.ejs', {data: users[index]});
   }
-
   res.render('./swipe.ejs', {data: users[index]});
 });
 
